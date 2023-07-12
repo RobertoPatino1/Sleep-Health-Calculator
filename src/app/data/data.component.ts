56897565f8bd66f6cb39abf4252d7fe977f8d5c7
Kevin-QQ-82 <kquintun@espol.edu.ec>
2023-07-12 14:24:55 -0500

    Added dependency injection logic

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Plantillas } from '../interfaces/plantillas';
+import { ServiciosService } from '../providers/servicios.service';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
 export class DataComponent {
+  public data:Plantillas[] = [];
+  constructor(private dataProvider:ServiciosService){
 
+  }
+  ngOnInit(){
+    this.dataProvider.getResponse().subscribe((response) => {
+      this.data = (response as Plantillas[]);
+    })
+  }
 }
