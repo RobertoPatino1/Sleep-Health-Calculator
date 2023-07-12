ec4407bca66a7842daebd490df6d5d15b8ee3f1b
Kevin-QQ-82 <kquintun@espol.edu.ec>
2023-07-12 14:06:10 -0500

    Agregado enlace a firebase

--- src/app/providers/servicios.service.ts
+++ src/app/providers/servicios.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+//Importación del HttpClient
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
-
-  constructor() { }
+  private URL: string = 'https://sleep-health-calculator-default-rtdb.firebaseio.com/collection.json';
+  constructor(private http:HttpClient) { }
 }
