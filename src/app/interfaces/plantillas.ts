2d93b992e02d6987eed995370da073a369d1081a
Kevin-QQ-82 <kquintun@espol.edu.ec>
2023-07-12 14:45:58 -0500

    data with ngfor

--- src/app/interfaces/plantillas.ts
+++ src/app/interfaces/plantillas.ts
@@ -1,2 +1,15 @@
 export interface Plantillas {
+    Age: string
+    "BMI Category": string
+    "Blood Pressure": string
+    "Daily Steps": string
+    Gender: string
+    "Heart Rate": string
+    Occupation: string
+    "Person ID": string
+    "Physical Activity Level": string
+    "Quality of Sleep": string
+    "Sleep Disorder": string
+    "Sleep Duration": string
+    "Stress Level": string
 }
