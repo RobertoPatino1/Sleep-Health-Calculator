c5422eb3b41db3dee8597baa0126fbd1064f6d66
RobertoPatino1 <rapatino@espol.edu.ec>
2023-07-24 02:34:04 -0500

    Finished welcome message component logic

--- src/app/salute-message/salute-message.component.ts
+++ src/app/salute-message/salute-message.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class SaluteMessageComponent{
   salute:string = "";
   timeIdentifier:number = 0
-  ngOnInit(): void {
-    const currentTime = new Date().getHours();
+  hour:number = 0;
+  minutes:number = 0;
+  formattedDate:string = ""
 
+  ngOnInit(): void {
+    this.setup()
+    const currentTime = new Date().getHours();
+    this.hour = new Date().getHours();
+    this.minutes = new Date().getMinutes();
     if(currentTime>=5 && currentTime<=12){
       this.salute = "Good Morning Walter!"
       this.timeIdentifier=1;
@@ -21,6 +27,27 @@ export class SaluteMessageComponent{
       this.salute = "Good Evening Walter!"
       this.timeIdentifier=3
     }
+  }
+
+  setup():void{
+    const weekDays = [
+      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    
+    // Array of the names of the months in English
+    const months = [
+      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+    // Get the current date
+    const currentDate = new Date();
+
+    // Get the day of the week, day of the month, month, and year
+    const dayOfWeek = weekDays[currentDate.getDay()];
+    const dayOfMonth = currentDate.getDate();
+    const month = months[currentDate.getMonth()];
+    const year = currentDate.getFullYear();
+
+    this.formattedDate = `${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`;
   }
   
 }
