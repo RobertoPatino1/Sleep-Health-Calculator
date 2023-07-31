9a5e858a43a81d7812c03dc8c496151956ce4e27
RobertoPatino1 <rapatino@espol.edu.ec>
2023-07-30 22:53:56 -0500

    Added chart config, still needs to be fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
+
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Sleep-calculator';
   imagenUrl: string = "";
-  imagenData: Blob|undefined;
+  imagenData: Blob | undefined;
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
-      this.setImage()
-      this.handleChallengesList()
+    this.setImage()
+    this.handleChallengesList()
+    this.plotLineChart()
   }
 
-  setImage():void{
+  setImage(): void {
     const url = "https://zenquotes.io/api/image";
     this.http.get(url, { responseType: 'blob' }).subscribe(
       (data: Blob) => {
@@ -30,19 +33,85 @@ export class AppComponent implements OnInit {
   }
 
 
-  handleChallengesList():void{
+  handleChallengesList(): void {
     // Add a "checked" symbol when clicking on a list item
     let list = document.querySelector('#challengesUL');
-    
-    if(list!=null){
-      list.addEventListener('click', function(ev) {
-        if(ev.target!=null)
+
+    if (list != null) {
+      list.addEventListener('click', function (ev) {
+        if (ev.target != null)
           (ev.target as HTMLElement).classList.toggle('checked');
-      },false);
+      }, false);
     }
+  }
 
+  plotLineChart(): void {
+    
+    const DATA_COUNT = 7;
+    const MONTHS = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    const labels = MONTHS.slice(0, 7);
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Unfilled',
+          fill: false,
+          backgroundColor: 'rgb(54, 162, 235)',
+          borderColor: 'rgb(54, 162, 235)'
+          // data: Utils.numbers(NUMBER_CFG),
+        }, {
+          label: 'Dashed',
+          fill: false,
+          backgroundColor: 'rgb(75, 192, 192)',
+          borderColor: 'rgb(75, 192, 192)',
+          borderDash: [5, 5]
+          // data: Utils.numbers(NUMBER_CFG),
+        }, {
+          label: 'Filled',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          // data: Utils.numbers(NUMBER_CFG),
+          fill: true,
+        }
+      ]
+    };
+    const config = {
+      type: 'line',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Chart.js Line Chart'
+          },
+        },
+        interaction: {
+          mode: 'index',
+          intersect: false
+        },
 
+      },
+    };
+    const ctx = document.getElementById("main-chart-area") as HTMLCanvasElement | null;
 
-
+    if (ctx !== null) {
+      const chart = new Chart(ctx, config);
+    } else {
+      console.error("Canvas element not found!");
+    }
   }
 }
