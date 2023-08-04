ce4aab9b0e8dfe90ab979c59728cc64e4e3d8f42
RobertoPatino1 <rapatino@espol.edu.ec>
2023-08-04 15:34:49 -0500

    Added table styling

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -8,16 +8,44 @@ import { ServiciosService } from '../providers/servicios.service';
 })
 export class DataComponent {
   public data:Plantillas[] = [];
-  @Input() category:string = "";
-  constructor(private dataProvider:ServiciosService){
+  public uniqueKeys:string[] = []
 
-  }
+
+  @Input() category:string = "";
+  constructor(private dataProvider:ServiciosService){}
+  
   ngOnInit(){
+    
     this.dataProvider.getResponse().subscribe((response) => {
       this.data = (response as Plantillas[]).slice(0,10);
-
     })
+    this.getUniqueKeys() 
+    console.log("--->");
+       
+    console.log(this.data);
+    console.log(this.category);
+    
+    
+  }
 
+  getUniqueKeys():void{    
+    if(this.category!=null){
+      if(this.category==="gender"){
+        //Fill the button with Male/Female options
+        this.uniqueKeys.push("Male");
+        this.uniqueKeys.push("Female");
+      }if(this.category==="BMI category"){
+        this.uniqueKeys.push("Normal")
+        this.uniqueKeys.push("Normal Weight")
+        this.uniqueKeys.push("Overweight")
+        this.uniqueKeys.push("Obese")
+      }else if(this.category==="occupation"){
+
+      }else if(this.category==="sleeping disorder"){
+
+      }
+    }
+     
   }
 
 
@@ -26,5 +54,11 @@ export class DataComponent {
     This function will query the dataset according to the category
     passed in as a parameter to the data tag
     */
+    if(this.category!=null){
+
+
+
+
+    }
   }
 }
