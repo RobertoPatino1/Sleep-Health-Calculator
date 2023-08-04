9a03cf18fea4eb678ea16caaa819fd9f371d99a8
unknown <nicmacor@espol.edu.ec>
2023-08-04 11:45:00 -0500

    calculadora

--- src/components/calculadora/calculadora.js
+++ src/components/calculadora/calculadora.js
@@ -6,9 +6,26 @@ exports.funcionPredeterminada = function () {
 
     let horasDeSueno = document.getElementById("horasDeSueno").value;
 
-    let resultado = (parseFloat(horasDeSueno) * 60) / 90;
+    let resultado = parseInt((parseFloat(horasDeSueno) * 60) / 90)
+    
 
-    document.getElementById("ciclos").value = parseInt(resultado);
+    if(resultado==5){
+      document.getElementById("ciclos").value = resultado + " cycles. Excellent, you are doing grate!";
+    }
+    else if(resultado==4){
+      document.getElementById("ciclos").value = resultado + " cycles. You should sleep a little more";
+    }
+    else if(resultado<4){
+      document.getElementById("ciclos").value = resultado + " cycles. You should sleep more";
+    }
+    else if(resultado==6){
+      document.getElementById("ciclos").value = resultado + " cycles. You are oversleeping a little";
+    }
+    else if(resultado>6){
+      document.getElementById("ciclos").value = resultado + " cycles. You are sleeping too much";
+    }
+      
+
   });
 };
 
