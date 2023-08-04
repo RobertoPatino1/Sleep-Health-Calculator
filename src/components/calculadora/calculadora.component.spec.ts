4280f3979f05abcdb5c21fc52e82f73f6c7f7693
unknown <nicmacor@espol.edu.ec>
2023-08-04 12:28:29 -0500

    tests

--- src/components/calculadora/calculadora.component.spec.ts
+++ src/components/calculadora/calculadora.component.spec.ts
@@ -2,12 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 
+//Importe el módulo cliente para requerimientos http
+import { HttpClientModule } from '@angular/common/http';
+
+//Importe el servicio
+import { ServiciosService } from 'src/app/providers/servicios.service';
+
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
   let fixture: ComponentFixture<CalculadoraComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      //Registre el módulo de petición http
+      imports: [ HttpClientModule ],
+
+      //Registre el servicio como proveedor de datos
+      providers: [ ServiciosService ],
+
       declarations: [CalculadoraComponent]
     });
     fixture = TestBed.createComponent(CalculadoraComponent);
@@ -17,6 +29,7 @@ describe('CalculadoraComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    
   });
 
 });
