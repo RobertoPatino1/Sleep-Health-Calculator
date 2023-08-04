31f465e92e92962353efbe78bad1beecab2221e9
RobertoPatino1 <rapatino@espol.edu.ec>
2023-08-03 22:14:40 -0500

    Changed user chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.setImage()
     this.handleChallengesList()
-    this.plotLineChart()
+    // this.plotLineChart()
     this.getDataFromService()
     this.getALLDataFromService()
     this.plotBarsChart()
@@ -63,40 +63,40 @@ this.plotScatterChart()
     }
   }
 
-  plotLineChart(): void {
-    var myChart = new Chart("main-chart-area", {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
+  // plotLineChart(): void {
+  //   var myChart = new Chart("main-chart-area", {
+  //     type: 'bar',
+  //     data: {
+  //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //         datasets: [{
+  //             label: '# of Votes',
+  //             data: [12, 19, 3, 5, 2, 3],
+  //             backgroundColor: [
+  //                 'rgba(255, 99, 132, 0.2)',
+  //                 'rgba(54, 162, 235, 0.2)',
+  //                 'rgba(255, 206, 86, 0.2)',
+  //                 'rgba(75, 192, 192, 0.2)',
+  //                 'rgba(153, 102, 255, 0.2)',
+  //                 'rgba(255, 159, 64, 0.2)'
+  //             ],
+  //             borderColor: [
+  //                 'rgba(255, 99, 132, 1)',
+  //                 'rgba(54, 162, 235, 1)',
+  //                 'rgba(255, 206, 86, 1)',
+  //                 'rgba(75, 192, 192, 1)',
+  //                 'rgba(153, 102, 255, 1)',
+  //                 'rgba(255, 159, 64, 1)'
+  //             ],
+  //             borderWidth: 1
+  //         }]
+  //     },
+  //     options: {
+  //         scales: {
 
-          }
-      }
-  });
-  }
+  //         }
+  //     }
+  // });
+  // }
 
 
   getDataFromService(): void {
--- src/components/graphs/graphs.component.ts
+++ src/components/graphs/graphs.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
-
+import {plotUserChart} from '../graphs/graphs.js'
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
   styleUrls: ['./graphs.component.css']
 })
 export class GraphsComponent {
-	
-	chartOptions = {
-		title: {
-		  text: "Basic Column Chart in Angular"
-		},
-		data: [{
-		  type: "column",
-		  dataPoints: [
-			{ label: "Apple",  y: 10  },
-			{ label: "Orange", y: 15  },
-			{ label: "Banana", y: 25  },
-			{ label: "Mango",  y: 30  },
-			{ label: "Grape",  y: 28  }
-		  ]
-		}]                
-	  };
+	ngOnInit(): void {
+		plotUserChart();
 	}
+}
 
 
 
